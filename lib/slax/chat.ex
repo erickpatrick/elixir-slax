d965a0e99ffeefa01bee819dcc18565aa648d9ea
erick patrick <github@erickpatrick.net>
2026-04-19 11:05:30 +0200

    uses Ecto.Query functional style to order room list

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -2,8 +2,12 @@ defmodule Slax.Chat do
   alias Slax.Chat.Room
   alias Slax.Repo
 
+  import Ecto.Query
+
   def list_rooms do
-    Repo.all(Room)
+    Room
+    |> order_by(asc: :name)
+    |> Repo.all()
   end
 
   def get_room!(id), do: Repo.get!(Room, id)
