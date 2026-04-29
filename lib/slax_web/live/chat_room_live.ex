f74bda23855981170358e363050499911cbc63d5
erick patrick <github@erickpatrick.net>
2026-04-29 22:51:57 +0200

    uses pubsub to add/remove messages and update everyonre in the chat

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -13,6 +13,8 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_params(params, _url, socket) do
+    if socket.assigns[:room], do: Chat.unsubscribe_from_room(socket.assigns.room)
+
     room =
       case Map.fetch(params, "id") do
         {:ok, id} -> Chat.get_room!(id)
@@ -20,6 +22,8 @@ defmodule SlaxWeb.ChatRoomLive do
       end
 
     messages = Chat.list_messages_in_room(room)
+
+    Chat.subscribe_to_room(room)
 
     {:noreply,
      socket
@@ -188,10 +192,8 @@ defmodule SlaxWeb.ChatRoomLive do
 
     socket =
       case Chat.create_message(room, message_params, current_scope) do
-        {:ok, message} ->
-          socket
-          |> stream_insert(:messages, message)
-          |> assign_message_form(Chat.change_message(%Message{}, %{}, current_scope))
+        {:ok, _message} ->
+          assign_message_form(socket, Chat.change_message(%Message{}, %{}, current_scope))
 
         {:error, changeset} ->
           assign_message_form(socket, changeset)
@@ -201,8 +203,16 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_event("delete-message", %{"id" => id}, socket) do
-    {:ok, message} = Chat.delete_message_by_id(id, socket.assigns.current_scope)
+    Chat.delete_message_by_id(id, socket.assigns.current_scope)
 
+    {:noreply, socket}
+  end
+
+  def handle_info({:message_created, message}, socket) do
+    {:noreply, stream_insert(socket, :messages, message)}
+  end
+
+  def handle_info({:message_deleted, message}, socket) do
     {:noreply, stream_delete(socket, :messages, message)}
   end
 
